510fb8dae983ddec7ee06bbd259e1e0e0aaaf25f
bonzoyang <bonzoyang@gmail.com>
2022-11-29 14:22:33 +0800

    add download.sh

---
+++ download.sh
@@ -0,0 +1,8 @@
+wget --content-disposition --directory-prefix ckpt/f2909158 https://www.dropbox.com/s/wqgsb6v64ap4rt1/all_results.json?dl=1
+wget --content-disposition --directory-prefix ckpt/f2909158 https://www.dropbox.com/s/pl5cqz15rd7cwod/config.json?dl=0
+wget --content-disposition --directory-prefix ckpt/f2909158 https://www.dropbox.com/s/s2kgli5oq8tv302/log.json?dl=1
+wget --content-disposition --directory-prefix ckpt/f2909158 https://www.dropbox.com/s/ziuo9hy39ky23s0/pytorch_model.bin?dl=1
+wget --content-disposition --directory-prefix ckpt/f2909158 https://www.dropbox.com/s/tmi6019kfd5vo7l/special_tokens_map.json?dl=1
+wget --content-disposition --directory-prefix ckpt/f2909158 https://www.dropbox.com/s/zviyglwqrtf0z9j/spiece.model?dl=1
+wget --content-disposition --directory-prefix ckpt/f2909158 https://www.dropbox.com/s/ij0pwghdwhiklrc/tokenizer_config.json?dl=1
+wget --content-disposition --directory-prefix ckpt/f2909158 https://www.dropbox.com/s/wa7p7tujrcjn2bi/tokenizer.json?dl=1
